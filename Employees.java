f68aae10af9f977db380ce14c3d3963143fb94d0
Lanmineree <suthata2550@gmail.com>
2023-06-03 17:32:55 +0700

    ok Co-authored-by: DusitaSungklinhom <DusitaSungklinhom@users.noreply.github.com>

--- Employees.java
+++ Employees.java
@@ -1,5 +1,36 @@
 public class Employees {
-    public String name;
-    public String id;
-    public Double salary;
+    //Attribute
+    private String name;
+    private String id;
+    private Double salary;
+    
+    //Default Constructor
+    public Employees(String id,String name,double salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+    //Method
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setSalary(Double salary){
+        this.salary = salary;
+    }
+    public void display(){
+        System.out.println("ID :"+this.id);
+        System.out.println("NAME :" + this.name);
+        System.out.println("SALARY :" + this.salary);
+
+    }
+    public String getName(){
+        return this.name;
+    }
+    public double getSalary(){
+        return this.salary;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,42 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         // format ชื่อ class blank ชื่อ object = new constructure
-        Employees emp1=new Employees();
-        Employees emp2=new Employees();
-        Employees emp3=new Employees();
-        Employees emp4=new Employees();
-        Employees emp5=new Employees();
+        
+        Employees e1 = new Employees("0","no name",0.0);
+        e1.display();
+        // Employees e2 = new Employees();
+        // e1.setName("Neree");
+        // e1.setId("1");
+        // e1.setSalary(30.0);
+        // e1.display();
 
-        emp1.id= "neree1234";
-        emp1.name= "Neree";
-        emp1.salary=100000.00;
-        emp2.id= "jeffrey2345";
-        emp2.name= "Jeffrey";
-        emp2.salary=88888.00;
-        emp3.id= "dion1234";
-        emp3.name= "Dion";
-        emp3.salary=120000.00;
-        emp4.id= "carlos1234";
-        emp4.name= "Carlos";
-        emp4.salary=30000.00;
-        emp5.id= "genvy1234";
-        emp5.name= "Genvy";
-        emp5.salary=22000.00;
-        System.out.println("Employee id: "+emp1.id);
-        System.out.println("Employee name: "+emp1.name);
-        System.out.println("Employee salary: "+emp1.salary);
-        System.out.println("Employee id: "+emp2.id);
-        System.out.println("Employee name: "+emp2.name);
-        System.out.println("Employee salary: "+emp2.salary);
-        System.out.println("Employee id: "+emp3.id);
-        System.out.println("Employee name: "+emp3.name);
-        System.out.println("Employee salary: "+emp3.salary);
-        System.out.println("Employee id: "+emp4.id);
-        System.out.println("Employee name: "+emp4.name);
-        System.out.println("Employee salary: "+emp4.salary);
-        System.out.println("Employee id: "+emp5.id);
-        System.out.println("Employee name: "+emp5.name);
-        System.out.println("Employee salary: "+emp5.salary);
+         
+        //  System.out.println(e1.getSalary());
+
+
+        // e2.setName("Dion");
+        // e2.setId("2");
+        // e2.setSalary(20.0);
+        // e2.display();
+
     }
 }
 //การบ้าน สร้าง object 5 object ให้ค่าแตกต่างกัน 
